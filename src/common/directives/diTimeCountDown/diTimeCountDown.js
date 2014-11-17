84bc3df94e728408b6cb043e163dbcda796cdd5f
daihua ye <daihua.ye@gmail.com>
2014-11-16 20:49:17 -0800

    add countdown code to show the count down animation effect

--- src/common/directives/diTimeCountDown/diTimeCountDown.js
+++ src/common/directives/diTimeCountDown/diTimeCountDown.js
@@ -4,10 +4,21 @@
 * Description
 */
 angular.module('direcitve.diTimeCountDown', [])
+.controller('TimeCountDownCtrl', [
+    '$scope',
+function (
+    $scope
+){
+
+}])
 .directive('diTimeCountDown', [
 function(
 ){
     var TimeCountDown = {};
+
+    TimeCountDown.controller = 'TimeCountDownCtrl';
+
+    TimeCountDown.templateUrl = 'directives/diTimeCountDown/diTimeCountDown.tpl.html';
 
     TimeCountDown.restrict = 'A';
 
@@ -16,8 +27,67 @@ function(
     TimeCountDown.scope = true;
 
     TimeCountDown.link = function link(scope, element, attrs) {
-        $(element).countdown('2015/10/10', function(event) {
-            $(this).html(event.strftime('%m Months %d days %H:%M:%S'));
+        var labels = ['MONTHS', 'DAYS', 'HOURS', 'MINUTES', 'SECONDS'],
+        nextYear = '2015/10/10',
+        template = _.template($(element).find('#countdown-template').html()),
+        currDate = '00:00:00:00:00',
+        nextDate = '00:00:00:00:00',
+        parser = /([0-9]{2})/gi,
+        $example = $(element);
+        // Parse countdown string to an object
+        function strfobj(str) {
+            var parsed = str.match(parser),
+                obj = {};
+            labels.forEach(function(label, i) {
+                obj[label] = parsed[i];
+            });
+            return obj;
+        }
+        // Return the time components that diffs
+        function diff(obj1, obj2) {
+            var diffAry = [];
+            labels.forEach(function(key) {
+                if (obj1[key] !== obj2[key]) {
+                    diffAry.push(key);
+                }
+            });
+            return diffAry;
+        }
+        // Build the layout
+        var initData = strfobj(currDate);
+        labels.forEach(function(label, i) {
+            $example.append(template({
+                curr: initData[label],
+                next: initData[label],
+                label: label
+            }));
+        });
+        // Starts the countdown
+        $example.countdown(nextYear, function(event) {
+            var newDate = event.strftime('%m:%d:%H:%M:%S'),
+                data;
+            if (newDate !== nextDate) {
+                currDate = nextDate;
+                nextDate = newDate;
+                // Setup the data
+                data = {
+                    'curr': strfobj(currDate),
+                    'next': strfobj(nextDate)
+                };
+                // Apply the new values to each node that changed
+                diff(data.curr, data.next).forEach(function(label) {
+                    var selector = '.%s'.replace(/%s/, label),
+                        $node = $example.find(selector);
+                    // Update the node
+                    $node.removeClass('flip');
+                    $node.find('.curr').text(data.curr[label]);
+                    $node.find('.next').text(data.next[label]);
+                    // Wait for a repaint to then flip
+                    _.delay(function($node) {
+                        $node.addClass('flip');
+                    }, 5, $node);
+                });
+            }
         });
     };
 
