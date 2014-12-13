0c6b76fa88111c46f825dcfb580e395e2adf0875
daihua ye <daihua.ye@gmail.com>
2014-12-13 15:22:39 -0800

    Don't show the street view if it is Device

--- src/common/directives/diGmap/diGmap.js
+++ src/common/directives/diGmap/diGmap.js
@@ -3,9 +3,13 @@
 *
 * Description
 */
-angular.module('directive.diGmap', [])
+angular.module('directive.diGmap', [
+    'service.Device'
+])
 .directive('diGmap', [
+    'Device',
 function(
+    Device
 ){
     var Gmap = {};
     
@@ -16,7 +20,47 @@ function(
     Gmap.replace = true;
 
     Gmap.link = function link(scope, element, attrs) {
-        var fenway = new google.maps.LatLng(37.405201, -122.149572);
+        var fenway = new google.maps.LatLng(37.405201, -122.149572),
+            markerOption = {
+                marker: {
+                    values: [{latLng: [37.405678, -122.149539]}],
+                    events:{
+                        click: openMarker,
+                        mouseover: openMarker
+                    }
+                }
+            },
+            mapOption = {
+                map:{
+                    options: {
+                        zoom: 17,
+                        mapTypeId: google.maps.MapTypeId.ROADMAP,
+                        streetViewControl: !Device.device,
+                        center: fenway,
+                        scrollwheel: false
+                    }
+                }
+            },
+            gmap3Option = {};
+
+        function getStreeViewOption() {
+            return {
+                streetviewpanorama: {
+                    options: {
+                        container: $(document.createElement("div")).addClass("googlemap col-md-6").attr('id', 'ra-streetview').insertAfter($(element)),
+                        opts: {
+                            position: fenway,
+                            scrollwheel: false,
+                            pov: {
+                                heading: -20,
+                                pitch: 5,
+                                zoom: 1
+                            }
+                        }
+                    }
+                }
+            };
+        }
 
         function openMarker(marker, event, context) {
             var map = $(this).gmap3("get"),
@@ -26,7 +70,7 @@ function(
                     }
                 }),
                 data = "<h5>University Club of Palo Alto</h5>" +
-                        "<p>3277 Miranda Ave Palo Alto, CA 94304</p>" +
+                        "<p><a href='http://goo.gl/0oJqRs' target='_blank'>3277 Miranda Ave Palo Alto, CA 94304</a></p>" +
                         "<p>(650) 493-3972</p>";
             if (infowindow) {
                 infowindow.open(map, marker);
@@ -41,38 +85,9 @@ function(
                 });
             }
         }
-        $(element).gmap3({
-            marker: {
-                values: [{latLng: [37.405678, -122.149539]}],
-                events:{
-                    click: openMarker,
-                    mouseover: openMarker
-                }
-            },
-            map:{
-                options: {
-                    zoom: 17,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP,
-                    streetViewControl: true,
-                    center: fenway,
-                    scrollwheel: false
-                }
-            },
-            streetviewpanorama: {
-                options: {
-                    container: $(document.createElement("div")).addClass("googlemap col-md-6").attr('id', 'ra-streetview').insertAfter($(element)),
-                    opts: {
-                        position: fenway,
-                        scrollwheel: false,
-                        pov: {
-                            heading: -20,
-                            pitch: 5,
-                            zoom: 1
-                        }
-                    }
-                }
-            }
-        });
+
+        angular.extend(gmap3Option, markerOption, mapOption, (!Device.device ? getStreeViewOption() : {}));
+        $(element).gmap3(gmap3Option);
     };
 
     return Gmap;
